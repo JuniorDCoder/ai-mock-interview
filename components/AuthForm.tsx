8df0e5ba1556be9001ee322b688e83d265c2abdd
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-05-02 22:24:43 +0100

    feat: Project files

---
+++ components/AuthForm.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+} from "@/components/ui/form"
+import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import { toast } from "sonner"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
+
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(5),
+    })
+}
+
+const AuthForm = (
+    { type }: {
+        type: FormType
+    }
+) => {
+
+    const router = useRouter()
+    const formSchema = authFormSchema(type)
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          name: "",
+            email: "",
+            password: "",
+        },
+      })
+     
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            if (type === "sign-in") {
+                toast.success("Sign in successful")
+                router.push("/")
+            } else {
+                toast.success("Account created successfully. Please sign in.")
+                router.push("/sign-in")
+            }
+        } catch (error) {
+            toast.error("There was an error: " + error)
+            console.log("Error", error)
+        }
+      }
+
+      const isSignIn = type === "sign-in"
+  return (
+    
+    <div className="card-border lg:min-w-[566px] lg:max-w-[566px]">
+        <div className="flex flex-col gap-6 card px-10 py-14">
+            <div className="flex flex-row gap-2 justify-center">
+                <Image src="/logo.svg" alt="logo" width={38} height={32} />
+                <h2 className="text-primary-100">PrepWise</h2>
+            </div>
+            <h3>Practice job inteviews with AI</h3>
+        
+            <Form {...form}>
+                <form 
+                onSubmit={form.handleSubmit(onSubmit)} 
+                className="space-y-6 w-full mt-4 form">
+                    {!isSignIn && 
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        label="Name"
+                        placeholder="Your Name"
+                    />
+                    }
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="Your Email Address"
+                        type="email"
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        label="Password"
+                        placeholder="Your Password"
+                        type="password"
+                    />
+                    
+                    <Button type="submit">
+                        {isSignIn ? "Sign In" : "Create an Account"}
+                    </Button>
+                </form>
+            </Form>
+            <p className="text-center">
+                {isSignIn
+                    ? "Don't have an account? "
+                    : "Already have an account? "}
+                <Link href={
+                    isSignIn ? "/sign-up" : "/sign-in"
+                } className="font-bold text-user-primary ml-1">
+                    {isSignIn ? "Sign Up" : "Sign In"}
+                </Link>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default AuthForm
